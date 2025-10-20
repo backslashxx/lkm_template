af422d0d3c7c85b3c947a9eeb14b3036018accf4
backslashxx <118538522+backslashxx@users.noreply.github.com>
2025-10-20 22:28:24 +0800

    userspace: remove basic_payload
    Signed-off-by: backslashxx <118538522+backslashxx@users.noreply.github.com>

--- userspace.c
+++ userspace.c
@@ -5,13 +5,6 @@
 #include <unistd.h>
 #include <stdint.h>
 
-// always use u64 for pointers regardless, this way we wont have any
-// issues like that nasty 32-on-64 pointer mismatch.
-struct basic_payload {
-	uint64_t reply_ptr;
-	char text[256];
-};
-
 int main(int argc, char *argv[]) {
 	if (argc != 4) {
 		printf("Usage: %s <option> <option> <string>\n", argv[0]);
@@ -21,20 +14,14 @@ int main(int argc, char *argv[]) {
 	unsigned long magic1 = strtoul(argv[1], NULL, 0);
 	unsigned long magic2 = strtoul(argv[2], NULL, 0);
 	const char *arg = argv[3];
-	
-	unsigned long reply_value = 0; // we get reply on this
 
-	struct basic_payload payload;
-	payload.reply_ptr = (uint64_t)&reply_value;
-	strncpy(payload.text, arg, sizeof(payload.text) - 1);
-	payload.text[sizeof(payload.text) - 1] = '\0';
+	printf("SYS_reboot(%lu, %lu, 0, %p)\n", magic1, magic2, arg);
+	syscall(SYS_reboot, magic1, magic2, 0, arg);
 
-	printf("SYS_reboot(%lu, %lu, 0, %p)\n", magic1, magic2, (void *)&payload);
-	syscall(SYS_reboot, magic1, magic2, 0, (void *)&payload);
-
-	// pointer reuse here
-	// force cast to uint to satisfy -Wpedantic
-	printf("reply: 0x%x\n", *(unsigned int *)&payload);
+	// try reusing the pointer
+	// since char is always already a pointer, we dont need to do
+	// *(cast *)&var deref nomenclature, *(cast *)var is enough
+	printf("reply: 0x%x\n", *(unsigned int *)arg);
 	
 	return 0;
 
